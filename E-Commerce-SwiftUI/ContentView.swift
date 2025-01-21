59aa4cb984a0b36fa35e02e69b9868665c5af444
Anket Kohak <anketkohak40@gmail.com>
2025-01-21 14:36:05 +0530

    Initial Commit

---
+++ E-Commerce-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  E-Commerce-SwiftUI
+//
+//  Created by Anket Kohak on 21/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ E-Commerce-SwiftUI/E_Commerce_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_SwiftUIApp.swift
+//  E-Commerce-SwiftUI
+//
+//  Created by Anket Kohak on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
